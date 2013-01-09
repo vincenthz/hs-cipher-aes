39e116b15add3f4d527f198128ed33cc84c6d902
Vincent Hanquez <vincent@snarc.org>
2013-01-09 21:46:34 +0000

    add a header

--- Crypto/Cipher/AES.hs
+++ Crypto/Cipher/AES.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE ViewPatterns #-}
+-- |
+-- Module      : Crypto.Cipher.AES
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : stable
+-- Portability : good
+--
 module Crypto.Cipher.AES
     (
     -- * data types
